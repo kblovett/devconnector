b31b6d8e88a5a1e831a25ec7ea728e7b7fa08afe
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-01-27 14:57:05 -0700

    rename profile route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => res.send('API Running'));
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/profiles', require('./routes/api/profiles'));
 app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
