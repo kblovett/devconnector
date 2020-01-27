31d059a1584b5a48e61ac88e396eaa41acaa669e
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-01-27 14:36:18 -0700

    add like route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -109,6 +109,31 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 // @route   GET api/posts/:userid
 // @desc    Get posts by ID
 // @access  Private
