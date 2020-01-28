51d6925547940e2ee95cf654736a502ba3f8c3e2
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-01-27 20:21:33 -0700

    initialize Register and Login

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
+        <a href='/'>
           <i className='fas fa-code'></i> DevConnector
         </a>
       </h1>
