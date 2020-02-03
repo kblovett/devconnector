d53b317e348055cdf0843ee515defe245335a158
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-02-02 20:23:49 -0700

    Profile creation

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -52,7 +52,8 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).send('Blargh');
+      //json({ errors: errors.array() });
     }
 
     const {
