226e0c294ebab30dcf00496b3052949cb10fa11b
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-02-03 00:11:01 -0700

    Profile edit

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import setAuthToken from './utils/setAuthToken';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -42,6 +43,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               ></PrivateRoute>
             </Switch>
           </section>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
@@ -12,7 +13,7 @@ const Dashboard = ({
 }) => {
   useEffect(() => {
     getCurrentProfile();
-  }, []);
+  }, [getCurrentProfile]);
 
   return loading && profile === null ? (
     <Spinner />
@@ -23,7 +24,9 @@ const Dashboard = ({
         <i className='fas fa-user' /> Welcome, {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet set up a profile. Please add some info:</p>
